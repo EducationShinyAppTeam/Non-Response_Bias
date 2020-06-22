116fbe1dbf6d0bf3caab45bafad9d180d9e8a6a7
jolin0504 <zpl5231@psu.edu>
2020-06-22 10:05:55 -0400

    First Version

---
+++ server.R
@@ -0,0 +1,243 @@
+library(shiny)
+library(ggplot2)
+library(mosaic)
+library(scales)
+library(Hmisc)
+library(DT)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  #Explore Button
+  observeEvent(input$explore, {
+    updateTabItems(session, "tabs", "UPRes")
+  })
+  #Null hypothesis
+  output$nullhypo = renderUI({
+    h3("Ho: p = 0.583")
+  })
+  
+  output$design = renderUI({
+    if (input$designcheckbox)
+    {
+      h4(
+        "A researcher plans to take a random sample of size n students to do a survey about their experiences in studying at the University Park campus of Penn State University. However, she worries that sample results could be biased because the students who agree to participate might be different from those who don't (this would be an example of non-response bias). The researcher makes a confidence interval for the percentage of Penn State Students who are Pennsylvania residents based on her study. This app shows  how confidence intervals of that type would come out when there is no bias."
+      )
+    }
+  })
+  
+  #population plot with true prop
+  output$popMean  = renderPlot({
+    my_vector = c(0.583, 1 - 0.583)
+    names(my_vector) = c("PA Students", "Non-PA Students")
+    ggplot() + geom_bar(
+      aes(x = names(my_vector), y = my_vector),
+      stat = 'identity',
+      width = 0.3,
+      fill = "steelblue"
+    ) +
+      lims(y = c(0, 1)) +
+      geom_hline(yintercept = 0.583,
+                 color = "forestgreen",
+                 size = 1.2) +
+      geom_hline(yintercept = input$prop,
+                 size = 1.2,
+                 colour = "orange") +
+      labs(
+        title = paste0("Study result is ", input$prop),
+        x = "Whether PA Resident",
+        y = "Proportion Enrollment by Residency"
+      )
+    #barplot(my_vector,col=rgb(0.2,0.4,0.6,0.6),ylim=c(0,1), ylab="precentage")
+    
+  })
+  
+  
+  
+  #Calculating alpha by the confidence level input
+  alpha <- reactive({
+    (1 - input$level)
+  })
+  
+  #Updating Sample Size
+  N <- reactive({
+    as.integer(input$nsamp)
+  })
+  
+  
+  #generate 50 new sample
+  Data <- reactive({
+    input$new
+    data.frame(x =
+                 do.call(paste0("rbinom"),
+                         c(
+                           list(n = as.integer(input$nsamp) * 50), list(1, input$prop)
+                         ))) %>%
+      mutate(idx = rep(1:50, each = input$nsamp))
+  })
+  
+  #calculate the interval
+  
+  
+  
+  Intervals <- reactive({
+    Data() %>%
+      group_by(idx) %>%
+      summarise(
+        Count = sum(x),
+        sampleProp = binconf(Count, N(), alpha = alpha())[1],
+        lowerbound = binconf(Count, N(), alpha = alpha())[2],
+        upperbound = binconf(Count, N(), alpha = alpha())[3],
+        cover = if_else(input$base == "True Proportion", ((lowerbound < input$prop) &
+                                                            (input$prop < upperbound)
+        ), ((lowerbound < 0.583) & (0.583 < upperbound)
+        ))
+      ) %>%
+      ungroup()
+  })
+  
+  
+  #default as all the samples are selected
+  selected_sample <- 50
+  selectedSample <- reactive({
+    if (!is.null(input$plot_click)) {
+      selected_sample <<- round(input$plot_click$y)
+      if (selected_sample < 1)
+        selected_sample <<- 1
+      if (selected_sample > 50)
+        selected_sample <<- 50
+    }
+    selected_sample
+  })
+  
+  OneSample <- reactive({
+    Data() %>%
+      filter(idx == selectedSample())
+  })
+  
+  OneSampleColor <- reactive({
+    colors <- c("TRUE" = "navy", "FALSE" = "red")
+    covers <-
+      (Intervals() %>% filter(idx == selectedSample()))$cover
+    colors[as.character(covers)]
+  })
+  
+  #print the CIplot
+  output$CIplot <- renderPlot({
+    validate(need(is.numeric(input$nsamp),
+                  message = "Please input sample size"))
+    
+    # validate(
+    #   need(input$nsamp >=30,
+    #        message = "Please input samle size larger than 30")
+    # )
+    
+    ggplot(data = Intervals()) +
+      geom_pointrange(
+        aes(
+          x = idx,
+          ymin = lowerbound,
+          ymax = upperbound,
+          y = sampleProp,
+          colour = cover,
+          alpha = idx == selectedSample(),
+          size = idx == selectedSample()
+        )
+      ) +
+      geom_hline(
+        yintercept = 0.583,
+        size = 2,
+        colour = "forestgreen",
+        alpha = 0.5
+      ) +
+      geom_hline(
+        yintercept = input$prop,
+        size = 2,
+        colour = "orange",
+        alpha = 0.5
+      ) +
+      coord_flip() +
+      scale_size_manual(values = c("TRUE" = 1.5, "FALSE" = .7),
+                        guide = FALSE) +
+      scale_color_manual(values = c("TRUE" = "navy", "FALSE" = "red"),
+                         guide = FALSE) +
+      scale_alpha_manual(values = c("TRUE" = 1, "FALSE" = .5),
+                         guide = FALSE) +
+      lims(y = c(-0.01, 1.01)) +
+      labs(
+        title = paste0(100 * input$level, "% Confidence Intervals for the proportion"),
+        x = "50 samples are generated every time",
+        y = "green vertical line shows null proportion, orange vertical line shows true proportion"
+      ) +
+      theme(
+        legend.position = "none",
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()
+      )
+  })
+  
+  output$sampProp  = renderPlot({
+    validate(need(is.numeric(input$nsamp),
+                  message = "Please input sample size"))
+    my_vector = c(round(mean(OneSample()$x), 2), 1 - round(mean(OneSample()$x), 2))
+    names(my_vector) = c("PA Students", "Non-PA Students")
+    ggplot() + geom_bar(
+      aes(x = names(my_vector), y = my_vector),
+      width = 0.3,
+      stat = 'identity',
+      fill = OneSampleColor()
+    ) +
+      lims(y = c(0, 1)) +
+      geom_hline(yintercept = 0.583,
+                 color = "forestgreen",
+                 size = 1.2) +
+      geom_hline(yintercept = input$prop,
+                 color = "orange",
+                 size = 1.2) +
+      labs(
+        title = paste0("Sample proportion for residency in UP = ",
+                       round(mean(OneSample(
+                       )$x), 2)),
+        x = "Whether PA Resident",
+        y = "Proportion Enrollment by Residency"
+      )
+    #barplot(my_vector,col=rgb(0.2,0.4,0.6,0.6),ylim=c(0,1), ylab="precentage")
+    
+  })
+  
+  rate <- reactiveValues(cover = 0, total = 0)
+  observeEvent(input$more, {
+    rate$cover <- sum(Intervals()$cover)
+    rate$total <- nrow(Intervals())
+  })
+  
+  
+  observeEvent(c(input$A, input$B, input$n, input$level),
+               {
+                 rate$cover <-
+                   sum(Intervals()$cover)
+                 rate$total <- nrow(Intervals())
+               })
+  
+  # text messages
+  output$CoverageRate <- renderText({
+    validate(need(is.numeric(input$nsamp),
+                  message = "Please input sample size"))
+    
+    paste(
+      sum(Intervals()$cover),
+      "of these",
+      nrow(Intervals()),
+      "intervals cover the parameter value. And coverage rate is ",
+      round(100 *  sum(Intervals()$cover) / nrow(Intervals()), 2),
+      "% (",
+      rate$total,
+      " samples)"
+    )
+  })
+  ############################################################
+  ############################################################
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,267 @@
+library(shiny)
+library(shinydashboard)
+library(shinyBS)
+
+
+dashboardPage(
+  skin = "purple",
+  
+  #Title
+  dashboardHeader(
+    title = "Non-response Bias",
+    titleWidth = 250,
+    tags$li(class = "dropdown", actionLink("info", icon("info"))),
+    tags$li(class = "dropdown",
+            tags$a(href='https://github.com/EducationShinyAppTeam/BOAST',
+                   icon("github"))),
+    tags$li(class = "dropdown",
+            tags$a(href='https://shinyapps.science.psu.edu/',
+                   icon("home")))
+  ),
+  
+  #Sidebar
+  dashboardSidebar(width = 250,
+                   sidebarMenu(
+                     id = "tabs",
+                     menuItem("Overview", tabName = "over", icon = icon("dashboard")),
+                     menuItem(
+                       "UP Residency Percentage",
+                       tabName = "UPRes",
+                       icon = icon("wpexplorer")
+                     ),
+                     menuItem("Reference",tabName = "Ref",icon = icon("leanpub"))),
+                     #PSU logo
+                     tags$div(
+                       class = "sidebar-logo",
+                       boastUtils::psu_eberly_logo("reversed")
+                     )
+                     
+                   ),
+  
+  #Content within the tabs
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", 
+                href="https://educationshinyappteam.github.io/Style_Guide/theme/boast.css")
+    ),
+    
+    
+    tabItems(
+      tabItem(
+        tabName = "over",
+        
+        
+        h1("Non-Response Bias"),
+        p(
+          "This app represents an interactive supplementary module embedded in statistics lessons with two features. The first feature visualizes how the variations in confidence levels and sample size affect the outcome confidence interval in a single mean. The second feature explores the non-response bias when considering true proportion."
+        ),
+        p(
+          "This lesson explores the behavior of confidence intervals for a single proportion, considering non-response bias."
+        ),
+        br(),
+        h2("Instructions"),
+       
+        tags$ol(
+          tags$li(
+            "Move the sample size and level sliders to see how they affect confidence intervals for the proportion of Penn State students who are residents of Pennsylvania  or two-sample tests for differences in the proportion of Pennsylvania residents between the University Park campus and the other campuses."
+          ),
+          tags$li(
+            "Move the true proportion slider to see the effects of non-response bias."
+          )
+        ),
+        
+        div(
+          style = "text-align: center",
+          bsButton(
+            "explore",
+            "Explore",
+            icon("bolt"),
+            size = "large",
+            class = "circle grow",
+            style = "default"
+          )
+        ),
+        br(),
+        h2("Acknowledgements:"),
+        
+        p(
+          "Information about confidence interval graph was drawn from Randall Pruim's shiny app."
+        ),
+        p(
+          "Budget Office in PSU. (2019), Enrollment by Residency Fall 2019. Available at https://factbook.psu.edu/factbook/StudentDynamic/PANonPASummary.aspx?YearCode=2019Enr&FBPlusIndc=N"
+        
+          ),
+        
+        helpText(
+          a("Click Here to view original official page",
+            href = "https://factbook.psu.edu/factbook/StudentDynamic/PANonPASummary.aspx?YearCode=2019Enr&FBPlusIndc=N",
+            target = "blank"
+          )
+        ),
+        br(),
+        p("We would like to extend a special thanks to the Shiny Program Students.",
+        br(),
+        br(),
+        br(),
+        div(class = "updated", "Last Update: 06/22/2020 by ZL.")
+        )
+      ),
+      
+      
+      tabItem(tabName = "UPRes",
+              fluidPage(
+                titlePanel(
+                  "Confidence Intervals for Enrollment by Residency in 2019 (p = 58.3%)"
+                ),
+                box(
+                  title = strong(tags$em("Design:")),
+                  # This is the header of the box. Consider using "Story Context"
+                  status = "primary",
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  width = '100%',
+                  "A researcher plans to take a random sample of size n students to do a survey about their experiences in studying at the University Park campus of Penn State University. However, she worries that sample results could be biased because the population of students who agree to participate might be different from those who don't (this would be an example of non-response bias). The researcher makes a confidence interval for the percentage of Penn State Students who are Pennsylvania residents based on her study and compares it to the null hypothesis mean of 59.5% for the population of all Penn State University Park students. This app shows how confidence intervals of that type would come out when there is bias so the proportion for the population of volunteer participants is really different."
+                )
+                ,
+                sidebarLayout(
+                  sidebarPanel(
+                    # h3(strong("Confidence Intervals for a population mean (n > 30): ")),
+                    # h4("For large random samples a confidence interval for a population mean is given by"),
+                    # h3("sample mean ± z* s / sqrt(n)"),
+                    # h4("where z* is a multiplier number that comes form the normal curve and determines the level of confidence."),
+                    
+                    #h4("A researcher plans to take a random sample of size n students to do a survey about their experiences in studying at the University Park campus of Penn State University. However, she worries that sample results could be biased because the students who agree to participate might be different from those who don't (this would be an example of non-response bias). The researcher makes a confidence interval for the percentage of Penn State Students who are Pennsylvania residents based on her study and compares it to the mean of 59.5% for the population of all Penn State University Park students. This app shows  how confidence intervals of that type would come out when there is no bias."),
+                    #h4("TIP: Click on an interval to show a histogram for the underlying sample."),
+                    h3(strong("Hypothesis: ")),
+                    uiOutput("nullhypo"),
+                    
+                    sliderInput(
+                      "prop",
+                      "True Proportion for Pennsylvania Students in UP",
+                      min = 0.01,
+                      max = 0.99,
+                      value = 0.583,
+                      step = 0.01
+                    ),
+                    
+                    sliderInput(
+                      "level",
+                      "Confidence Level",
+                      min = .50,
+                      max = 0.99,
+                      value = 0.90,
+                      step = 0.01
+                    ),
+                    sliderInput(
+                      "nsamp",
+                      "Sample Size (n > 30)",
+                      min = 30,
+                      max = 500,
+                      value = 30,
+                      step = 5
+                    ),
+                    
+                    selectInput(
+                      "base",
+                      "Select Base of CI Color",
+                      selected = "True Proportion",
+                      choices = c("Null Hypothesis Proportion", "True Proportion")
+                    ),
+                    
+                    actionButton("new", "Generate 50 New Samples", icon("retweet")),
+                    bsPopover(
+                      "new",
+                      "Note",
+                      "By clicking on this button, new 50 sample with the size you have input in each sample will be generated.",
+                      trigger = "hover",
+                      placement = "right"
+                    ),
+                    br(),
+                    br()
+                    
+                    
+                    
+                  ),
+                  
+                  mainPanel(
+                    fluidRow(
+                      splitLayout(
+                        cellWidths = c("50%", "50%"),
+                        plotOutput("popMean", height = "350px"),
+                        plotOutput("sampProp", height = "350px")
+                      ),
+                      wellPanel(
+                        plotOutput("CIplot", height = "600px", click = "plot_click"),
+                        textOutput("CoverageRate")
+                      ),
+                      br(),
+                      bsPopover(
+                        "sampProp",
+                        "Sample Histogram Plot",
+                        "This is the histogram plot of the sample you selected on Confidence Interval Plot. The orange line is the true percentage that you input.",
+                        trigger = "hover",
+                        placement = "top"
+                      ),
+                      br(),
+                      
+                      bsPopover(
+                        "popMean",
+                        "Population Bar Graph",
+                        "This is the bar plot based on percentage you input. The green line is the null hypothesis perventage and alsp the true percentage in 2016.",
+                        trigger = "hover",
+                        placement = "bottom"
+                      ),
+                      br(),
+                      
+                      bsPopover(
+                        "CIplot",
+                        "Confidence Interval Plot",
+                        "The blue lines indicate a confidence interval covers the proportion selected and the red lines indicate that the selected proportion is outside of the confidence interval. Click on an interval to show a histogram for the underlying sample.",
+                        trigger = "hover",
+                        placement = "top"
+                      ),
+                      br(),
+                      
+                      
+                      tags$head(
+                        tags$style(
+                          "#CoverageRate{color: green;
+                                 font-size: 18px;
+                                                     font-style: italic;
+                                                     }"
+                        )
+                      )
+                    )
+                  )
+                )
+              )),
+      
+      tabItem(
+        tabName = "Ref",
+        withMathJax(),
+        h2("References"),
+        p(class = "hangingindent",
+          "Bailey, E. (2015), shinyBS: Twitter bootstrap components for shiny, R package. Available from https://CRAN.R-project.org/package=shinyBS"),
+        p(class = "hangingindent",
+          "Budget Office in PSU. (2019), Enrollment by Residency Fall 2019. Available at https://factbook.psu.edu/factbook/StudentDynamic/PANonPASummary.aspx?YearCode=2019Enr&FBPlusIndc=N"),
+        p(class = "hangingindent",
+          "Chang, W. and Borges Ribeio, B. (2018), shinydashboard: Create dashboards with 'Shiny', R Package. Available from https://CRAN.R-project.org/package=shinydashboard"),
+        p(class = "hangingindent",
+          "Chang, W., Cheng, J., Allaire, J., Xie, Y., and McPherson, J. (2019), shiny: Web application framework for R, R Package. Available from https://CRAN.R-project.org/package=shiny"),
+        p(class = "hangingindent",
+          "Harrell Jr, F., Dupont C., and many others. (2020), Hmisc: Harrell miscellaneous, R Package. Available from https://CRAN.R-project.org/package=Hmisc"),
+        p(class = "hangingindent",
+          "Pruim, R., Kaplan, D., and Horton, N. (2020), mosaic: Project MOSAIC statistics and mathematics teaching utilities, R Package. Avaliable from https://CRAN.R-project.org/package=mosaic"),
+        p(class = "hangingindent",
+          "Wickham, H. (2016), ggplot2: Elegant graphics for data analysis, R Package, New York: Springer-Verlag. Available from https://ggplot2.tidyverse.org"),
+        p(class = "hangingindent",
+          "Wickham, H., Seidel, D., and R Studio. (2020), scales: Scale function for visualization, R Package. Availabel from https://CRAN.R-project.org/package=scales"),
+        p(class = "hangingindent",
+          "Xie, Y. (2020), DT: A wrapper of the JavaScript library 'DataTables', R Package. Available from https://CRAN.R-project.org/package=DT")
+      )
+  
+    )#end of tabItem
+  )#end of dashboardBody
+  
+)
+
