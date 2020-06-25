be9d7b7dc886bca3ed9dc64b28da77b9bb080261
jolin0504 <zpl5231@psu.edu>
2020-06-25 08:05:49 -0400

    add function binconf()

--- server.R
+++ server.R
@@ -6,6 +6,88 @@ library(ggplot2)
 library(DT)
 
 
+# Helper Functions -------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#  Title: Confidence Intervals for Binomial Probability
+#  Author: Frank E. Harrell, Jr.
+#  Date: 2020
+#  Code version: 4.4-0
+#  Availability: https://CRAN.R-project.org/package=Hmisc
+#  We needed to bypass the loading of the foreign package for R 3.6.3, thus
+#  we are using the definition of the binconf which is all we needed from Hmisc.
+#-------------------------------------------------------------------------------
+binconf <- function (x, n, alpha = 0.05,
+                     method = c("wilson", "exact", "asymptotic", "all"),
+                     include.x = FALSE, include.n = FALSE, return.df = FALSE)
+{
+  method <- match.arg(method)
+  bc <- function(x, n, alpha, method) {
+    nu1 <- 2 * (n - x + 1)
+    nu2 <- 2 * x
+    ll <- if (x > 0)
+      x/(x + qf(1 - alpha/2, nu1, nu2) * (n - x + 1))
+    else 0
+    nu1p <- nu2 + 2
+    nu2p <- nu1 - 2
+    pp <- if (x < n)
+      qf(1 - alpha/2, nu1p, nu2p)
+    else 1
+    ul <- ((x + 1) * pp)/(n - x + (x + 1) * pp)
+    zcrit <- -qnorm(alpha/2)
+    z2 <- zcrit * zcrit
+    p <- x/n
+    cl <- (p + z2/2/n + c(-1, 1) * zcrit *
+             sqrt((p * (1 - p) + z2/4/n)/n))/(1 + z2/n)
+    if (x == 1)
+      cl[1] <- -log(1 - alpha)/n
+    if (x == (n - 1))
+      cl[2] <- 1 + log(1 - alpha)/n
+    asymp.lcl <- x/n - qnorm(1 - alpha/2) * sqrt(((x/n) *
+                                                    (1 - x/n))/n)
+    asymp.ucl <- x/n + qnorm(1 - alpha/2) * sqrt(((x/n) *
+                                                    (1 - x/n))/n)
+    res <- rbind(c(ll, ul), cl, c(asymp.lcl, asymp.ucl))
+    res <- cbind(rep(x/n, 3), res)
+    switch(method, wilson = res[2, ], exact = res[1, ], asymptotic = res[3,
+                                                                         ], all = res, res)
+  }
+  if ((length(x) != length(n)) & length(x) == 1)
+    x <- rep(x, length(n))
+  if ((length(x) != length(n)) & length(n) == 1)
+    n <- rep(n, length(x))
+  if ((length(x) > 1 | length(n) > 1) & method == "all") {
+    method <- "wilson"
+    warning("method=all will not work with vectors...setting method to wilson")
+  }
+  if (method == "all" & length(x) == 1 & length(n) == 1) {
+    mat <- bc(x, n, alpha, method)
+    dimnames(mat) <- list(c("Exact", "Wilson", "Asymptotic"),
+                          c("PointEst", "Lower", "Upper"))
+    if (include.n)
+      mat <- cbind(N = n, mat)
+    if (include.x)
+      mat <- cbind(X = x, mat)
+    if (return.df)
+      mat <- as.data.frame(mat)
+    return(mat)
+  }
+  mat <- matrix(ncol = 3, nrow = length(x))
+  for (i in 1:length(x)) mat[i, ] <- bc(x[i], n[i], alpha = alpha,
+                                        method = method)
+  dimnames(mat) <- list(rep("", dim(mat)[1]), c("PointEst",
+                                                "Lower", "Upper"))
+  if (include.n)
+    mat <- cbind(N = n, mat)
+  if (include.x)
+    mat <- cbind(X = x, mat)
+  if (return.df)
+    mat <- as.data.frame(mat, row.names = NULL)
+  mat
+}
+#End of Harrell's code----------------------------------------------------------
+
+
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   #Explore Button
